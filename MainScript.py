98e00402b8691f2f84511329590a84c25767b6f9
GT_Console <1029550448@qq.com>
2019-03-16 13:31:54 +0800

    UpData

--- MainScript.py
+++ MainScript.py
@@ -5,9 +5,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import re
+from Tool import *
 
 #UA设置
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0'}
 
 def pUrl(url):#查找省份链接
     req = request.Request(url, headers=headers)
@@ -21,9 +22,9 @@ def pUrl(url):#查找省份链接
     for i in find_url:
         pUrl.append('https://' + str(i.get("href"))[2:])
         pList.append(i.getText())
-    info = pList, pUrl  # 城市名 链接
-    print(info)
-    return info  # 返回城市名和链接
+    info = pList, pUrl  # 省份名 链接
+    #print(info)
+    return info  # 返回省份名和链接
 
 def cityUrlLoad(url):#通过省份链接查找城市链接
     req = request.Request(url, headers=headers)
@@ -38,7 +39,7 @@ def cityUrlLoad(url):#通过省份链接查找城市链接
         cityUrl.append('https://'+str(i.get("href"))[2:])
         cityList.append(i.getText())
     info=cityList,cityUrl#城市名 链接
-    print(info)
+    #print(info)
     return info#返回城市名和链接
 
 def hUrl(url):#通过城市链接查找医院链接
@@ -54,7 +55,7 @@ def hUrl(url):#通过城市链接查找医院链接
         hUrl.append('https://www.haodf.com/'+str(i.get("href"))[2:])
         hList.append(i.getText())
     info = hList, hUrl  # 医院名 链接
-    print(info)
+    #print(info)
     return info  # 返回医院名和链接
 
 def doctorUrlList(url):  # 获取推荐医生列表页面
@@ -89,16 +90,17 @@ def doctorList(url):#获取医生链接列表
     find_a=find_a.findAll(attrs={"class": "blue"})
     doctorList=[]
     for i in find_a:
-        doctorList.append(str(i.get("href"))[2:])
-    print(doctorList)
+        doctorList.append("https://"+str(i.get("href"))[2:])
     return doctorList
 
 def doctorinfo(url):
-    url="http://"+url
     req = request.Request(url, headers=headers)
     html = urlopen(req)
     html_BSObj = BeautifulSoup(html, "lxml")  # 链接对象
+    findTittle=(str(html_BSObj.title.text).split("_"))[0]#获取标题
+    find_info = html_BSObj.findAll(attrs={"id": "bp_doctor_share"})#获取详细评论
 
+    print(find_info)
 
 
 '''
@@ -111,21 +113,25 @@ print("OUT_INFO:浏览器启动成功！")
 
 def __init__():
     def debug():
-        url="https://www.haodf.com/yiyuan/all/list.htm"
-        pUrl(url)
         '''
+        url="https://www.haodf.com/yiyuan/all/list.htm"
+        purlList=pUrl(url)
+
+
+
         url='https://www.haodf.com/yiyuan/beijing/list.htm'
         cityUrlLoad(url)
         url='https://www.haodf.com/yiyuan/beijing/chaoyang/list.htm'
         hUrl(url)
-        
+
         url = 'https://www.haodf.com/hospital/DE4raCNSz6OmG3OUNZWCWNv0.htm'
         doctorUrlList(url)
         url='http://www.haodf.com/tuijian/DE4raCNSz6OmG3OUNZWCWNv0/daizhuangpaozhen.htm'
-        doctorList(url)
-        url='www.haodf.com/doctor/DE4r0eJWGqZNZNSkwlZUkj5SMh1oor0a.htm'
+        doctorList(url)'''
+
+
+        url='https://www.haodf.com/doctor/DE4rO-XCoLUmy1568JOrYZEIRi.htm'
         doctorinfo(url)
-        '''
     debug()
 
 __init__()
---
+++ Tool.py
@@ -0,0 +1,10 @@
+
+
+def GPInfo(info):
+    print("INFO:",info)
+
+def GPError(int,info):
+    print("===ERROR===",int,':',info)
+
+def GPAct(info):
+    print("ACT:",info,"ing...")
